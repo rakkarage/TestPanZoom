b600559a9224d889611c6069a4fa7e8808e209a1
Rakka Rage <rakkarage@gmail.com>
2023-06-24 15:43:11 -0400

    x

--- Test.gd
+++ Test.gd
@@ -1,7 +1,6 @@
 extends SubViewport
 
 @onready var _camera: Camera2D = $Camera2D
-@onready var _tileMap: TileMap = $TileMap
 
 const _zoomMin := 0.2
 const _zoomMinVector := Vector2(_zoomMin, _zoomMin)
@@ -18,7 +17,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			_zoom(event.global_position, _zoomFactorVector)
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
 			_zoom(event.global_position, -_zoomFactorVector)
-		elif event.button_index == MOUSE_BUTTON_LEFT:
+		elif event.button_index == MOUSE_BUTTON_LEFT or event.button_index == MOUSE_BUTTON_MIDDLE:
 			_panning = event.pressed
 	elif event is InputEventMouseMotion and _panning:
 			_pan(event.relative)
@@ -26,20 +25,11 @@ func _unhandled_input(event: InputEvent) -> void:
 func _pan(delta: Vector2) -> void:
 	_camera.global_position -= delta / _camera.zoom
 
-func _zoom(position: Vector2, factor: Vector2) -> void:
-	printMapMousePosition()
-	var z0 := _camera.zoom
-	print(z0)
-	var z1 = (z0 + factor).clamp(_zoomMinVector, _zoomMaxVector)
-	print(z1)
-	var c0 := _camera.global_position
-	print(c0)
-	var c1 = c0 + position * (z0 - z1)
-	print(c1)
-	_camera.zoom = z1
-	_camera.global_position = c1
-	printMapMousePosition()
-
-func printMapMousePosition() -> void:
-	var mousePosition := _tileMap.to_local(_camera.get_global_mouse_position())
-	print(mousePosition)
+func _zoom(at: Vector2, factor: Vector2) -> void:
+	var zoomOld := _camera.zoom
+	var zoomNew = (zoomOld + factor).clamp(_zoomMinVector, _zoomMaxVector)
+	var center := Vector2(size) / 2.0 # same as: _camera.get_viewport().get_visible_rect().size / 2.0
+	var positionOld := _camera.global_position
+	var positionNew = positionOld + ((at - center) / zoomOld + (center - at) / zoomNew)
+	_camera.zoom = zoomNew
+	_camera.global_position = positionNew
