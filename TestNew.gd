35e320ea5c6a1db180bfdf47045c48f91be8e9ac
Rakka Rage <rakkarage@gmail.com>
2023-06-24 21:07:17 -0400

    _pan_momentum

--- TestNew.gd
+++ TestNew.gd
@@ -2,32 +2,39 @@ extends Node2D
 
 @onready var _camera: Camera2D = $Camera2D
 
-const _zoomMin := 0.2
-const _zoomMinVector := Vector2(_zoomMin, _zoomMin)
-const _zoomMax := 16.0
-const _zoomMaxVector := Vector2(_zoomMax, _zoomMax)
-const _zoomFactor := 0.1
-const _zoomFactorBase := 10.0
+const _zoom_min := 0.2
+const _zoom_min_vector := Vector2(_zoom_min, _zoom_min)
+const _zoom_max := 16.0
+const _zoom_max_vector := Vector2(_zoom_max, _zoom_max)
+const _zoom_factor := 0.1
+const _zoom_factor_base := 10.0
 
 var _panning := false
+var _pan_momentum := Vector2.ZERO
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed:
-			_zoom(event.global_position, _zoomFactor)
+			_zoom(event.global_position, _zoom_factor)
 		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed:
-			_zoom(event.global_position, -_zoomFactor)
+			_zoom(event.global_position, -_zoom_factor)
 		elif event.button_index == MOUSE_BUTTON_LEFT or event.button_index == MOUSE_BUTTON_MIDDLE:
 			_panning = event.pressed
 	elif event is InputEventMouseMotion and _panning:
 			_pan(event.relative)
 
+func _process(_delta: float) -> void:
+	if not _panning:
+		_camera.global_position -= _pan_momentum / _camera.zoom
+		_pan_momentum = _pan_momentum.lerp(Vector2.ZERO, 0.1)
+
 func _pan(delta: Vector2) -> void:
+	_pan_momentum = delta
 	_camera.global_position -= delta / _camera.zoom
 
 func _zoom(at: Vector2, factor: float) -> void:
-	var zoomOld := _camera.zoom
-	var zoomNew = (zoomOld * pow(_zoomFactorBase, factor)).clamp(_zoomMinVector, _zoomMaxVector)
-	_camera.zoom = zoomNew
+	var zoom_old := _camera.zoom
+	var zoom_new = (zoom_old * pow(_zoom_factor_base, factor)).clamp(_zoom_min_vector, _zoom_max_vector)
+	_camera.zoom = zoom_new
 	var center := _camera.get_viewport().get_visible_rect().size / 2.0
-	_camera.global_position += ((at - center) / zoomOld + (center - at) / zoomNew)
+	_camera.global_position += ((at - center) / zoom_old + (center - at) / zoom_new)
